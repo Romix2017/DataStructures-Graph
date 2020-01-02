bde4a49005a9b347baccc11bd577a45dd6f3aaff
Roman Karpov <poma.05@mail.ru>
2020-01-02 15:38:13 +0400

    init

---
+++ Vertex.cs
@@ -0,0 +1,16 @@
+namespace Graph
+{
+    class Vertex
+    {
+        public int Number { get; set; }
+        public Vertex(int number)
+        {
+            Number = number;
+        }
+
+        public override string ToString()
+        {
+            return Number.ToString();
+        }
+    }
+}
